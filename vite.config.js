8513c82068c8c2aa05d9e02b4d33821de717e3b3
Santiago <santiagogomezromerowork@gmail.com>
2026-01-03 13:27:08 -0500

    fix: Correct base path to match repository name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/informe-rituales-nuevo/',
+  base: '/informe-rituales-2025/',
 })
